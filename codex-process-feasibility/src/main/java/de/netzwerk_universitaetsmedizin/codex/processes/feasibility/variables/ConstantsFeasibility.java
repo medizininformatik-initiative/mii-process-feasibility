bb39d66cee4d5e707f88ecb3a9634a08fc09a9a9
Alexander Kiel <alexanderkiel@gmx.net>
2021-02-01 18:18:46 +0100

    Fix FHIR Conformance Resource URL's
    I used http://netzwerk-universitaetsmedizin.de instead of https://www.netzwerk-universitaetsmedizin.de as used in GECCO.

--- codex-process-feasibility/src/main/java/de/netzwerk_universitaetsmedizin/codex/processes/feasibility/variables/ConstantsFeasibility.java
+++ codex-process-feasibility/src/main/java/de/netzwerk_universitaetsmedizin/codex/processes/feasibility/variables/ConstantsFeasibility.java
@@ -8,11 +8,11 @@ public interface ConstantsFeasibility {
     String VARIABLE_MEASURE_REPORT_ID = "measure-report-id";
     String VARIABLE_AGGREGATED_MEASURE_REPORT = "count";
 
-    String CODESYSTEM_FEASIBILITY = "http://netzwerk-universitaetsmedizin.de/fhir/CodeSystem/feasibility";
+    String CODESYSTEM_FEASIBILITY = "https://www.netzwerk-universitaetsmedizin.de/fhir/CodeSystem/feasibility";
     String CODESYSTEM_FEASIBILITY_VALUE_MEASURE_REFERENCE = "measure-reference";
     String CODESYSTEM_FEASIBILITY_VALUE_MEASURE_REPORT_REFERENCE = "measure-report-reference";
     String CODESYSTEM_FEASIBILITY_VALUE_SINGLE_RESULT = "single";
     String CODESYSTEM_FEASIBILITY_VALUE_AGGREGATED_RESULT = "aggregated";
 
-    String EXTENSION_DIC_URI = "http://netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/dic";
+    String EXTENSION_DIC_URI = "https://www.netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/dic";
 }
