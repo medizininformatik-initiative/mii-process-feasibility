4d70ab6e67a22eb24f727d40bcecd6029fc68df7
DiCanio <TwrdikAlex@aol.com>
2022-03-24 10:37:37 +0100

    Use Correct Process Path in Test Setup Script
    Ensures usage of the correct process path within the test
    setup build script.

--- feasibility-dsf-process-docker-test-setup/rebuild.sh
+++ feasibility-dsf-process-docker-test-setup/rebuild.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-mvn -f ../feasibility-dsf-process-bpe-plugin/pom.xml clean package
+mvn -f ../feasibility-dsf-process/pom.xml clean package
 mvn -f ../feasibility-dsf-process-tools/feasibility-dsf-process-test-data-generator/pom.xml clean package
