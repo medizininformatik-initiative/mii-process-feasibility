8afa7bb1bd1eb725d2d0d2ed46606fafbb79308f
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2022-04-22 09:06:41 +0200

    set read access of meassure report only for requester

--- feasibility-dsf-process/src/test/java/de/medizininformatik_initiative/feasibility_dsf_process/service/StoreMeasureReportTest.java
+++ feasibility-dsf-process/src/test/java/de/medizininformatik_initiative/feasibility_dsf_process/service/StoreMeasureReportTest.java
@@ -1,51 +1,70 @@
 package de.medizininformatik_initiative.feasibility_dsf_process.service;
 
-import de.medizininformatik_initiative.feasibility_dsf_process.service.StoreMeasureReport;
+import static de.medizininformatik_initiative.feasibility_dsf_process.variables.ConstantsFeasibility.VARIABLE_MEASURE_REPORT;
+import static de.medizininformatik_initiative.feasibility_dsf_process.variables.ConstantsFeasibility.VARIABLE_MEASURE_REPORT_ID;
+import static org.highmed.dsf.bpe.ConstantsBase.BPMN_EXECUTION_VARIABLE_LEADING_TASK;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.highmed.dsf.fhir.authorization.read.ReadAccessHelper;
+import org.highmed.dsf.fhir.authorization.read.ReadAccessHelperImpl;
 import org.highmed.dsf.fhir.client.FhirWebserviceClientProvider;
 import org.highmed.fhir.client.FhirWebserviceClient;
 import org.highmed.fhir.client.PreferReturnMinimalWithRetry;
 import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.MeasureReport;
+import org.hl7.fhir.r4.model.Reference;
+import org.hl7.fhir.r4.model.Task;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static de.medizininformatik_initiative.feasibility_dsf_process.variables.ConstantsFeasibility.VARIABLE_MEASURE_REPORT;
-import static de.medizininformatik_initiative.feasibility_dsf_process.variables.ConstantsFeasibility.VARIABLE_MEASURE_REPORT_ID;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 @RunWith(MockitoJUnitRunner.StrictStubs.class)
-public class StoreMeasureReportTest {
+public class StoreMeasureReportTest
+{
 
-    @Mock
-    private FhirWebserviceClientProvider clientProvider;
+	@Mock
+	private FhirWebserviceClientProvider clientProvider;
 
-    @Mock
-    private FhirWebserviceClient localWebserviceClient;
+	@Mock
+	private FhirWebserviceClient localWebserviceClient;
 
-    @Mock
-    private PreferReturnMinimalWithRetry returnMinimal;
+	@Mock
+	private PreferReturnMinimalWithRetry returnMinimal;
 
-    @Mock
-    private DelegateExecution execution;
+	@Mock
+	private DelegateExecution execution;
 
-    @InjectMocks
-    private StoreMeasureReport service;
+	@Mock
+	private ReadAccessHelper readAccessHelper;
 
-    @Test
-    public void testDoExecute() {
-        when(clientProvider.getLocalWebserviceClient()).thenReturn(localWebserviceClient);
-        when(localWebserviceClient.withMinimalReturn()).thenReturn(returnMinimal);
-        MeasureReport measureReport = new MeasureReport();
-        when(returnMinimal.create(measureReport)).thenReturn(new IdType("id-094601"));
-        when(execution.getVariable(VARIABLE_MEASURE_REPORT)).thenReturn(measureReport);
+	@InjectMocks
+	private StoreMeasureReport service;
 
-        service.doExecute(execution);
+	@Test
+	public void testDoExecute() throws Exception
+	{
+		MeasureReport measureReport = new MeasureReport();
 
-        verify(execution).setVariable(VARIABLE_MEASURE_REPORT_ID, "id-094601");
-    }
+		Task task = new Task();
+		Reference requesterReference = new Reference().setIdentifier(
+				new Identifier().setSystem("http://localhost/systems/sample-system").setValue("requester-id"));
+		task.setRequester(requesterReference);
+
+		when(clientProvider.getLocalWebserviceClient()).thenReturn(localWebserviceClient);
+		when(localWebserviceClient.withMinimalReturn()).thenReturn(returnMinimal);
+		when(returnMinimal.create(measureReport)).thenReturn(new IdType("id-094601"));
+		when(execution.getVariable(VARIABLE_MEASURE_REPORT)).thenReturn(measureReport);
+		when(execution.getVariable(BPMN_EXECUTION_VARIABLE_LEADING_TASK)).thenReturn(task);
+		when(readAccessHelper.addOrganization(measureReport, "requester-id")).thenReturn(
+				new ReadAccessHelperImpl().addOrganization(measureReport, "requester-id"));
+
+		service.execute(execution);
+
+		verify(execution).setVariable(VARIABLE_MEASURE_REPORT_ID, "id-094601");
+	}
 }
