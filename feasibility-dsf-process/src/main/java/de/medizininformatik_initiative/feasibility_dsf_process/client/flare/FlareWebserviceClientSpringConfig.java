4a6e313986ba3c7cf6629f58668db6bc31b492b2
Mathias Rühle <math2306@hotmail.com>
2023-11-22 18:59:10 +0100

    Allow Basic Authentication for Flare Store Client

--- feasibility-dsf-process/src/main/java/de/medizininformatik_initiative/feasibility_dsf_process/client/flare/FlareWebserviceClientSpringConfig.java
+++ feasibility-dsf-process/src/main/java/de/medizininformatik_initiative/feasibility_dsf_process/client/flare/FlareWebserviceClientSpringConfig.java
@@ -2,7 +2,12 @@ package de.medizininformatik_initiative.feasibility_dsf_process.client.flare;
 
 import de.medizininformatik_initiative.feasibility_dsf_process.client.store.TlsClientFactory;
 import de.medizininformatik_initiative.feasibility_dsf_process.spring.config.BaseConfig;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +17,8 @@ import org.springframework.context.annotation.Import;
 import java.net.URI;
 
 import javax.net.ssl.SSLContext;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 @Configuration
 @Import(BaseConfig.class)
@@ -23,6 +30,12 @@ public class FlareWebserviceClientSpringConfig {
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.flare.timeout.connect:2000}")
     private int connectTimeout;
 
+    @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.auth.basic.username:#{null}}")
+    private String basicAuthUsername;
+
+    @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.auth.basic.password:#{null}}")
+    private String basicAuthPassword;
+
     @Bean
     public FlareWebserviceClient flareWebserviceClient(HttpClient httpClient) {
         return new FlareWebserviceClientImpl(httpClient, URI.create(flareBaseUrl));
@@ -30,8 +43,15 @@ public class FlareWebserviceClientSpringConfig {
 
     @Bean
     public HttpClient flareHttpClient(@Qualifier("base-client") SSLContext sslContext) {
-        return new TlsClientFactory(null, sslContext)
-                .getNativeHttpClientBuilder()
-                .build();
+        HttpClientBuilder builder = new TlsClientFactory(null, sslContext).getNativeHttpClientBuilder();
+
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+
+        if (!isNullOrEmpty(basicAuthUsername) && !isNullOrEmpty(basicAuthPassword)) {
+            URI flareUri = URI.create(flareBaseUrl);
+            credentialsProvider.setCredentials(new AuthScope(new HttpHost(flareUri.getHost(), flareUri.getPort())),
+                    new UsernamePasswordCredentials(basicAuthUsername, basicAuthPassword));
+        }
+        return builder.setDefaultCredentialsProvider(credentialsProvider).build();
     }
 }
