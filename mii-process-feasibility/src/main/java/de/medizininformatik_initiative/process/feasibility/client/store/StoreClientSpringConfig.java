f4c7bf1baf68aeb32a3b6517b25f0ca390173827
Mathias Rühle <math2306@hotmail.com>
2025-03-17 00:53:38 +0100

    Move Documentation to GitHub Repository Wiki

--- mii-process-feasibility/src/main/java/de/medizininformatik_initiative/process/feasibility/client/store/StoreClientSpringConfig.java
+++ mii-process-feasibility/src/main/java/de/medizininformatik_initiative/process/feasibility/client/store/StoreClientSpringConfig.java
@@ -8,6 +8,7 @@ import ca.uhn.fhir.rest.client.interceptor.BasicAuthInterceptor;
 import ca.uhn.fhir.rest.client.interceptor.BearerTokenAuthInterceptor;
 import de.medizininformatik_initiative.process.feasibility.EvaluationStrategy;
 import de.medizininformatik_initiative.process.feasibility.spring.config.BaseConfig;
+import dev.dsf.bpe.v1.documentation.ProcessDocumentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -31,57 +32,118 @@ public class StoreClientSpringConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(StoreClientSpringConfig.class);
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "Proxy host, set if the server containing the FHIR data or the Flare service can only be reached through a proxy",
+            example = "proxy.example.com")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.proxy.host:#{null}}")
     private String proxyHost;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "Proxy port, set if the server containing the FHIR data or the Flare service can only be reached through a proxy",
+            example = "8080")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.proxy.port:}")
     private Integer proxyPort;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "Proxy username, set if the server containing the FHIR data or the Flare service can only be reached through a proxy which requests authentication")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.proxy.username:#{null}}")
     private String proxyUsername;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "Proxy password, set if the server containing the FHIR data or the Flare service can only be reached through a proxy which requests authentication")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.proxy.password:#{null}}")
     private String proxyPassword;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "Bearer token for authentication, set if the server containing the FHIR data or the Flare service requests authentication using a bearer token")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.auth.bearer.token:#{null}}")
     private String bearerAuthToken;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "Basic authentication username, set if the server containing the FHIR data or the Flare service requests authentication using basic auth")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.auth.basic.username:#{null}}")
     private String basicAuthUsername;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "Basic authentication password, set if the server containing the FHIR data or the Flare service requests authentication using basic auth")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.auth.basic.password:#{null}}")
     private String basicAuthPassword;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "Timeout in milliseconds for the store client to establish a connection to the server containing the FHIR data")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.timeout.connect:2000}")
     private Integer connectTimeout;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "Timeout in milliseconds for requesting a connection from the connection pool for the store client")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.timeout.connect_request:20000}")
     private Integer connectRequestTimeout;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "Timeout in milliseconds the store client waits for a response from the server containing the FHIR data")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.timeout.socket:" + CLIENT_TIMEOUT_DEFAULT + "}")
     private Integer socketTimeout;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "The base address of the server containing the FHIR data",
+            required = true,
+            example = "http://foo.bar/fhir")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.base_url:}")
     private String storeBaseUrl;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "OAuth2.0 client secret, set if the server containing the FHIR data requests authentication using OAuth2.0")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.auth.oauth.client.password:#{null}}")
     private String oauthClientSecret;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "OAuth2.0 client id, set if the server containing the FHIR data requests authentication using OAuth2.0")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.auth.oauth.client.id:#{null}}")
     private String oauthClientId;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "OAuth2.0 issuer url, set if the server containing the FHIR data requests authentication using OAuth2.0",
+            example = "https://auth.example.org/realms/dic-data")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.auth.oauth.issuer.url:#{null}}")
     private String oauthIssuerUrl;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "OAuth2.0 proxy host, set if the server containing the FHIR data requests authentication using OAuth2.0 which requires a proxy",
+            example = "proxy.example.org")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.auth.oauth.proxy.host:#{null}}")
     private String oauthProxyHost;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "OAuth2.0 proxy port, set if the server containing the FHIR data requests authentication using OAuth2.0 which requires a proxy",
+            example = "8080")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.auth.oauth.proxy.port:}")
     private Integer oauthProxyPort;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "OAuth2.0 proxy username, set if the server containing the FHIR data requests authentication using OAuth2.0 which requires a proxy with basic authentication")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.auth.oauth.proxy.username:#{null}}")
     private String oauthProxyUsername;
 
+    @ProcessDocumentation(
+            processNames = { "medizininformatik-initiativede_feasibilityExecute" },
+            description = "OAuth2.0 proxy password, set if the server containing the FHIR data requests authentication using OAuth2.0 which requires a proxy with basic authentication")
     @Value("${de.medizininformatik_initiative.feasibility_dsf_process.client.store.auth.oauth.proxy.password:#{null}}")
     private String oauthProxyPassword;
 
